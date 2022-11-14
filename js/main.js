106e992c685da44aa9efb23bc7ec2259a42b73a3
Leanne Vu <leannevu1@yahoo.com>
2022-11-14 23:35:20 +0000

    Utilize API and JSON to populate list with musiical genres upon click #1.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var $musicList = document.querySelector('#music-list');
+var $exploreButton = document.querySelector('.gen-but');
+function getUserData() {
+  var $list = document.querySelectorAll('li');
+  for (var z = 0; z < $list.length; z++) {
+    $list[z].remove();
+  }
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://binaryjazz.us/wp-json/genrenator/v1/genre/3');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    for (var i = 0; i < xhr.response.length; i++) {
+      var $li = document.createElement('li');
+      $li.textContent = xhr.response[i];
+      $musicList.appendChild($li);
+    }
+
+  }); xhr.send();
+}
+$exploreButton.addEventListener('click', getUserData);
