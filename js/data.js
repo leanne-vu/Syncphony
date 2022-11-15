5b8bd7df920660070fbb7a49b793c08884162284
Leanne Vu <leannevu1@yahoo.com>
2022-11-15 22:07:31 +0000

    Add a sibling data-view with the value selections ini the HTML. . Add dummy selections to unordered list. #1

--- js/data.js
+++ js/data.js
@@ -1,1 +1,14 @@
 /* exported data */
+var data = {
+  view: 'generator',
+  genre: []
+};
+var previousDataJSON = localStorage.getItem('genre-storage');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('genre-storage', dataJSON);
+});
